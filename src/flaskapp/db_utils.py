de406a711c55774334fd18a9133619201f03d083
YOONJIN <yj0819@g.skku.edu>
2020-07-18 14:20:24 +0900

    firebase와 연동시 필요

---
+++ src/flaskapp/db_utils.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate('mykey.json')
+firebase_admin.initialize_app(cred,{'databaseURL':'https://heven-t.firebaseio.com/'})
+
+
+ref=db.reference()
+ref.update({'날씨' : 'good'})
+
+
+
+
+
