8a1213295e5be47e60801c7832fa42ff399e1572
Unknown <cookkyh99@skku.edu>
2020-07-31 00:43:25 +0900

    오타 수정 및 줄맞춤

--- src/flaskapp/static/js/mainpage.js
+++ src/flaskapp/static/js/mainpage.js
@@ -22,28 +22,27 @@ $( document ).ready(function() {
 
     // 주소로 좌표를 검색합니다
     geocoder.addressSearch( locate , function(result, status) {
+         // 정상적으로 검색이 완료됐으면
+         if (status === kakao.maps.services.Status.OK) {
 
-    // 정상적으로 검색이 완료됐으면
-     if (status === kakao.maps.services.Status.OK) {
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
 
-        // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: coords
-        });
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            var infowindow = new kakao.maps.InfoWindow({
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">여기인가요?</div>'
+            });
+            infowindow.open(map, marker);
 
-        // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
-            content: '<div style="width:150px;text-align:center;padding:6px 0;">여기인가요?</div>'
-        });
-        infowindow.open(map, marker);
-
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-        map.setCenter(coords);
-    }
-});
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        }
+    });
 
 
     /* -- 현재 접속 위치 받아오기 --*/
@@ -70,8 +69,8 @@ $( document ).ready(function() {
                 // 마커 위치를 클릭한 위치로 옮깁니다
                 marker.setPosition(latlng);
 
-                let message = '클릭한 위치의 위도 : ' + latlng.getLat() + /n;
-                message += '경도 : ' + latlng.getLng() ;
+                let message = '클릭한 위치의 위도 : ' + latlng.getLat();
+                message += '경도 : ' + latlng.getLng();
 
                 $('#location').text(message);
 
