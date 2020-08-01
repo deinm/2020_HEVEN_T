4c8d864af8871ccdea4154912505b8532934d28a
Unknown <cookkyh99@skku.edu>
2020-08-01 13:09:57 +0900

    update db_utils to use another package

--- src/flaskapp/db_utils.py
+++ src/flaskapp/db_utils.py
@@ -1,31 +1,21 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from flaskapp import db
 
-cred = credentials.Certificate('mykey.json')
-firebase_admin.initialize_app(cred,{'databaseURL':'https://heven-t.firebaseio.com/'})
-
-
-ref = db.reference()
-ref.update({'날씨' : 'good'})
 
 def get_CO2():
-    ref_CO2 = db.reference('CO2/12가3456/CO2')
-    CO2 = ref_CO2.get()
-    return CO2
+    ref_CO2 = db.child("CO2").child("12가3456").child("CO2").get().val()
+    return ref_CO2
     threading.Timer(300, self.get_CO2).start()
 
 
 def get_lat():
-    ref_GPS = db.reference('car_gps/12가3456/GPS')
-    GPS = ref_GPS.get()
-    lat=GPS.split(',')[0]
+    ref_GPS = db.child("car_gps").child("12가3456").child("GPS").get().val()
+    lat = ref_GPS.split(',')[0]
     return lat
     threading.Timer(300, self.get_lat).start()
 
+
 def get_lon():
-    ref_GPS = db.reference('car_gps/12가3456/GPS')
-    GPS = ref_GPS.get()
-    lon=GPS.split(',')[1]
+    ref_GPS = db.child("car_gps").child("12가3456").child("GPS").get().val()
+    lon = ref_GPS.split(',')[1]
     return lon
     threading.Timer(300, self.get_lon).start()
